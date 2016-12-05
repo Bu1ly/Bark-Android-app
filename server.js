90a6565dc708a703ac39e6cb0bdedfbd5fdad27b
Danny Volpov <volpovdanny89@gmail.com>
2016-12-05 15:42:54 +0200

    changing bin/www file to integrate with openSHift

---
+++ server.js
@@ -0,0 +1,15 @@
+var ipaddress = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
+var port      = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+
+var http = require('http');
+
+var server = http.createServer(function(request, response) {
+    console.log((new Date()) + ' Received request for ' + request.url);
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+    response.write("Welcome to Node.js on OpenShift!\n\n");
+    response.end("Thanks for visiting us! \n");
+});
+
+server.listen( port, ipaddress, function() {
+    console.log((new Date()) + ' Server is listening on port 8080');
+});
