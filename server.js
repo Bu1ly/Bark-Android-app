14cc78217122b1251eb64257cccd12cd9f63f2e0
Danny Volpov <volpovdanny89@gmail.com>
2016-12-05 15:53:08 +0200

    changing bin/www file to integrate with openSHift

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
 var port      = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 
+var WebSocketServer = require('ws').Server
 var http = require('http');
 
 var server = http.createServer(function(request, response) {
@@ -13,3 +14,18 @@ var server = http.createServer(function(request, response) {
 server.listen( port, ipaddress, function() {
     console.log((new Date()) + ' Server is listening on port 8080');
 });
+
+wss = new WebSocketServer({
+    server: server,
+    autoAcceptConnections: false
+});
+wss.on('connection', function(ws) {
+    console.log("New connection");
+    ws.on('message', function(message) {
+        ws.send("Received: " + message);
+    });
+    ws.send('Welcome!');
+});
+
+console.log("Listening to " + ipaddress + ":" + port + "...");
+
