6b115184256a3f73b453e93a1fea421763ff71b2
assaf <assafas@gmail.com>
2017-01-08 17:26:55 +0200

    update android app

--- android/Barkandroid/app/src/main/java/com/example/danny/barkandroid/MainActivity.java
+++ android/Barkandroid/app/src/main/java/com/example/danny/barkandroid/MainActivity.java
@@ -1,13 +1,30 @@
 package com.example.danny.barkandroid;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.login);
+        textView=(TextView)findViewById(R.id.link_signup);
+
+        textView.setOnClickListener(new View.OnClickListener()
+        {
+
+            public void onClick(View v)
+            {
+            startActivity(new Intent(MainActivity.this,register.class));
+            }
+
+        });
     }
 }
